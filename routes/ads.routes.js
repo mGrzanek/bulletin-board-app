9251b42340c05deac3477db6c2913afb4fad90a8
Monika Grzanek <grzanekmonika@o2.pl>
2025-04-22 15:05:23 +0200

    Update ads routes with authMiddleware

--- routes/ads.routes.js
+++ routes/ads.routes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const AdsController = require('./../controllers/ads.controller');
+const authMiddleware = require('./../utils/authMiddleware');
 const imageUpload = require('./../utils/imageUpload');
 
 router.get('/ads', AdsController.getAll);
 router.get('/ads/search/:searchPhrase', AdsController.getSearchPhrase);
 router.get('/ads/:id', AdsController.getOne);
-router.post('/ads', imageUpload.single('image'), AdsController.addNew);
-router.put('/ads/:id', imageUpload.single('image'), AdsController.editOne);
-router.delete('/ads/:id', AdsController.removeOne);
+router.post('/ads', authMiddleware, imageUpload.single('image'), AdsController.addNew);
+router.put('/ads/:id', authMiddleware, imageUpload.single('image'), AdsController.editOne);
+router.delete('/ads/:id', authMiddleware, AdsController.removeOne);
 
 module.exports = router;
