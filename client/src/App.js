96353f13e6f72c51f07769b5f877ef640f409209
Monika Grzanek <grzanekmonika@o2.pl>
2025-04-23 13:07:56 +0200

    Add Home and NotFound components and update App with routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,16 @@
+import { Container } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home/Home";
+import NotFound from "./components/pages/NotFound/NotFound";
+
 const App = () => {
   return (
-    <h1>Hello</h1>
+    <Container>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Container>
   );
 };
 
