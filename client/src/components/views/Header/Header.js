2a3fdfe45c3d215380a0543a05944b27263d964d
Monika Grzanek <grzanekmonika@o2.pl>
2025-04-24 09:54:25 +0200

    Add NavBar component

--- client/src/components/views/Header/Header.js
+++ client/src/components/views/Header/Header.js
@@ -1,10 +1,9 @@
-import styles from './Header.module.scss';
 import Hero from "../Hero/Hero";
 import NavBar from "../NavBar/NavBar";
 
 const Header = () => {
     return(
-       <div className={styles.header}>
+       <div>
             <Hero />
             <NavBar />
        </div>
--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -1,6 +1,19 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+
 const NavBar = () => {
     return(
-        <h2>NavBar</h2>
+        <Navbar variant="dark" className="bg-black bg-gradient">
+            <Container className="d-flex justify-content-around">
+                <Nav>
+                    <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+                </Nav>
+                <Nav>
+                    <Nav.Link as={NavLink} to="/register">Sing up</Nav.Link>
+                    <Nav.Link as={NavLink} to="/login">Sign in</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
     );
 }
 
