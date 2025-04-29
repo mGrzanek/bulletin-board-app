55da104440be6ed79e30942407089ccc747699e2
Monika Grzanek <grzanekmonika@o2.pl>
2025-04-29 15:44:52 +0200

    Update styles for AdItem

--- client/src/components/views/AdItem/AdItem.js
+++ client/src/components/views/AdItem/AdItem.js
@@ -1,15 +1,17 @@
 import { Card, Col, Button } from "react-bootstrap";
+import clsx from 'clsx';
+import styles from './AdItem.module.scss';
 import { NavLink } from "react-router-dom";
 import { IMG_URL } from "../../../config";
 
 const AdItem = ({ _id, title, image, location }) => {
     return(
         <Col xs={12} md={6} lg={4} className="pt-4">
-            <Card className="p-3 shadow border-warning-subtle rounded">
+            <Card className={clsx(styles.card, "p-3 shadow border-warning-subtle rounded")}>
                 <Card.Body>
-                <Card.Img variant="top" src={IMG_URL + image} />
-                <Card.Title className="text-warning pt-2">{title}</Card.Title>
-                <Card.Text><b>Location: </b> {location}</Card.Text>
+                <Card.Img variant="top" src={IMG_URL + image} className={styles.cardImage} />
+                <Card.Title className={clsx(styles.cardTitle, "text-warning py-2")}>{title}</Card.Title>
+                <Card.Text className="py-1"><b>Location: </b> {location}</Card.Text>
                 <Button variant="outline-warning"  size="sm" className=" text-lowercase fw-bold" as={NavLink} to={`/ads/${_id}`}>
                     Read more
                 </Button>
